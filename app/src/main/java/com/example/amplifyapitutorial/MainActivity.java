a2b3be53340655d3f77cdd29df51a3f34bad04c6
Liam Fell <liam.haylands@hotmail.co.uk>
2020-06-08 22:23:03 +0100

    Added api call

--- app/src/main/java/com/example/amplifyapitutorial/MainActivity.java
+++ app/src/main/java/com/example/amplifyapitutorial/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.amplifyapitutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.amplifyframework.api.rest.RestOptions;
+import com.amplifyframework.core.Amplify;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RestOptions options = RestOptions.builder()
+                .addPath("/todo")
+                .addBody("{\"name\":\"Mow the lawn\"}".getBytes())
+                .build();
+
+        Amplify.API.post(options,
+                response -> Log.i("MyAmplifyApp", "POST " + response.getData().asString()),
+                error -> Log.e("MyAmplifyApp", "POST failed", error)
+        );
     }
 }
